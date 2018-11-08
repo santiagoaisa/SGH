032630889d7c805dc1260b8aaa6ac636dc20d9cf
wjoo20 <44094088+wjoo20@users.noreply.github.com>
2018-11-08 16:39:22 -0500

    clases2

--- EntidadesNegocio/Historia.cs
+++ EntidadesNegocio/Historia.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Entities
 {
-    class Historia
+    public class Historia
     {
+        public string CodEsp { get; set; }
+        public DateTime FechaAper { get; set; }
+        public double Peso { get; set; }
+        public double Talla { get; set; }
+        public Historia(string codEsp, DateTime fechaAper, double peso, double talla)
+        {
+            this.CodEsp = codEsp;
+            this.FechaAper = fechaAper;
+            this.Peso = peso;
+            this.Talla = talla;
+        }
     }
 }
