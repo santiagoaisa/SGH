fb6de91a38f210a13c52c4f5dbfb676d6c753dad
wjoo20 <44094088+wjoo20@users.noreply.github.com>
2018-11-08 16:39:05 -0500

    clases2

--- EntidadesNegocio/Medico.cs
+++ EntidadesNegocio/Medico.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EntidadesNegocio
+namespace Entities
 {
-    class Medico
+    public class Medico : Persona
     {
+        public string Cmp { get; set; }
+        public EnumEspecialidad Espec { get; set; }
+        public Medico(string dni, string nombre, string apellidos, string cmp, EnumEspecialidad espec) : base(dni, nombre, apellidos)
+        {
+            this.Cmp = cmp;
+            this.Espec = espec;
+        }
+
+    
     }
 }
