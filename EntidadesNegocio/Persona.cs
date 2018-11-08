7a00c5bf52caaafca5c966d3945319a111151065
wjoo20 <44094088+wjoo20@users.noreply.github.com>
2018-11-08 16:37:48 -0500

    clases2

--- EntidadesNegocio/Persona.cs
+++ EntidadesNegocio/Persona.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace Entities
 {
-    class Persona
+    public class Persona
     {
+        public string Dni { get; set; }
+
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+
+        public Persona(string dni, string nombre, string apellidos)
+        {
+            this.Dni = dni;
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+        }
     }
 }
