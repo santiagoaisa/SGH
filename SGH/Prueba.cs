50deb7acd97f38e8c5f6e346dc253fe07caeda3e
Sixto Santiago Aisa Incapuño <santiagoaisa@gmail.com>
2018-11-09 11:30:53 -0500

    Logica de negocios

---
+++ SGH/Prueba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+using LogicaNegocios;
+
+namespace SGH
+{
+    class Prueba
+    {
+        static void Main(string[] args)
+        {
+            PacienteBL pacienteBL = new PacienteBL();
+         
+            Paciente newPaciente = new Paciente("44216619", "SIXTO SANTIAGO", "AISA INCAPUÑO", DateTime.Parse("06-08-2018"),EnumTipo.ASEGURADO);
+        
+            
+            pacienteBL.InsertPaciente(newPaciente);
+            Console.WriteLine("Insertar Paciente...");
+
+        }
+       
+    }
+}
