202d570cc65070530cbf58041898f43587d46781
BrandonMunoz <soldadodelcaos10@gmail.com>
2018-11-28 11:58:51 -0500

    luego de tiempo

--- AccesoDatos/MedicamentosDAL.cs
+++ AccesoDatos/MedicamentosDAL.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace AccesoDatos
 {
     class ConexionMedicamentos
     {
+        string pathBDDoctores = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        return pathBDDoctores;
     }
 }
--- EntidadesNegocio/Cita.cs
+++ EntidadesNegocio/Cita.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace EntidadesNegocio
 {
-    class CIta
+    public class Cita
     {
+        public string DNI_Paciente { get; set; }
+        public string Nombres { get; set; }
+        public double Apellidos { get; set; }
+        public double COD_Especialidad { get; set; }
+        public double COD_Doctor { get; set; }
+        public Cita(string codEsp, DateTime fechaAper, double peso, double talla)
+        {
+            this.CodEsp = codEsp;
+            this.FechaAper = fechaAper;
+            this.Peso = peso;
+            this.Talla = talla;
+        }
     }
 }
